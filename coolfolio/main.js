0d335c39f8123d3d39f683e8a03307f4a8fdd1c8
AineeJames <olsenaiden33@gmail.com>
2022-04-18 21:11:01 -0700

    good classes

--- coolfolio/main.js
+++ coolfolio/main.js
@@ -1,15 +1,57 @@
 import './style.css';
+import sevseg from './sevseg_font.json?url';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
-import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass.js';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 
-import sevseg from './sevseg_font.json?url';
+class Planet {
+  constructor(geomtype, size, color, trail) {
+    var g = null;
+    if (geomtype == 'icosahedron') {
+      g = new THREE.IcosahedronGeometry(size, 0);
+    } else if (geomtype == 'dodecahedron') {
+      g = new THREE.DodecahedronGeometry(size, 0);
+    } else if (geomtype == 'tetrahedron') {
+      g = new THREE.TetrahedronGeometry(size, 0);
+    } else if (geomtype == 'box') {
+      g = new THREE.BoxGeometry(size, size, size);
+    }
+    const m = new THREE.MeshStandardMaterial({color: color});
+    this.mesh = new THREE.Mesh(g, m);
+    scene.add(this.mesh);
+
+    if (trail == true) {
+      this.lpoints = [];
+      const g = new THREE.BufferGeometry().setFromPoints( this.lpoints );
+      const m = new THREE.LineBasicMaterial({color: color});
+      this.l = new THREE.Line(g, m);
+      scene.add(this.l);
+    }
+  }
+  rotate(x, y, z) {
+    this.mesh.rotation.x += x;
+    this.mesh.rotation.y += y;
+    this.mesh.rotation.z += z;
+  }
+  orbit(mag, trigx, trigy, trigz, tick, speedmod) {
+    this.mesh.position.x = (trigx == 'sin') ? mag*Math.sin(tick/speedmod) : mag*Math.cos(tick/speedmod);
+    this.mesh.position.y = (trigy == 'sin') ? mag*Math.sin(tick/speedmod) : mag*Math.cos(tick/speedmod);
+    this.mesh.position.z = (trigz == 'sin') ? mag*Math.sin(tick/speedmod) : mag*Math.cos(tick/speedmod);
+  }
+  updateTrail(maxlength) {
+    this.lpoints.push(new THREE.Vector3(this.mesh.position.x, this.mesh.position.y, this.mesh.position.z));
+    this.l.geometry = new THREE.BufferGeometry().setFromPoints(this.lpoints);
+    scene.add(this.l);
+    if (this.lpoints.length >= maxlength) {
+      this.lpoints.shift();
+    }
+  }
+}
 
 // Basic scene, camera, and renderer setup here
 const scene = new THREE.Scene();         //FOV, aspect ratio, view frustrum
@@ -31,28 +73,12 @@ composer.addPass( renderPass );
 
 const bloomPass = new UnrealBloomPass(1, 2, 0,);
 composer.addPass(bloomPass);
-const filmPass = new FilmPass(0.1, 0.05, 1000, 0);
-//composer.addPass(filmPass);
 
-const geometry = new THREE.IcosahedronGeometry(15, 0);
-const material = new THREE.MeshStandardMaterial({color: 0xF59342});
-const sun = new THREE.Mesh(geometry, material);
-scene.add(sun);
-
-const egeometry = new THREE.DodecahedronGeometry(4, 0);
-const ematerial = new THREE.MeshStandardMaterial({color: 0x3C81C9});
-const earth = new THREE.Mesh(egeometry, ematerial);
-scene.add(earth);
-
-const geomx = new THREE.TetrahedronGeometry(8, 1 );
-const matx = new THREE.MeshStandardMaterial({color: 0xAA3139});
-const planetx = new THREE.Mesh(geomx, matx);
-scene.add(planetx);
-
-const geomt = new THREE.BoxGeometry(3, 3, 3);
-const matt = new THREE.MeshStandardMaterial({color: 0x6e6e6e});
-const planett = new THREE.Mesh(geomt, matt);
-scene.add(planett);
+/*   ---===Planet Definitions===---   */
+const sun = new Planet('icosahedron', 15, 0xF59342, false);
+const earth = new Planet('dodecahedron', 4, 0x3C81C9, true);
+const planetx = new Planet('tetrahedron', 8, 0xAA3139, true);
+const tiny = new Planet('box', 3, 0x6E6E6E, true);
 
 const pointLight = new THREE.PointLight(0x6e6e6e);
 pointLight.position.set(20,10,5);
@@ -62,8 +88,6 @@ scene.add(pointLight, ambientLight);
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 //scene.add(lightHelper, gridHelper);
-
-//const controls = new OrbitControls(camera, renderer.domElement);
 
 function addBlip() {
   var randscale = Math.floor(Math.random() * 2) + 1
@@ -80,23 +104,6 @@ function addBlip() {
 }
 Array(1000).fill().forEach(addBlip);
 
-const elinematerial = new THREE.LineBasicMaterial({color: 0x3c81c9});
-const epoints = [];
-const elinegeometry = new THREE.BufferGeometry().setFromPoints( epoints );
-const eline = new THREE.Line( elinegeometry, elinematerial );
-scene.add(eline);
-
-const xlinematerial = new THREE.LineBasicMaterial({color: 0xAA3139});
-const xpoints = [];
-const xlinegeometry = new THREE.BufferGeometry().setFromPoints( xpoints );
-const xline = new THREE.Line( xlinegeometry, xlinematerial );
-scene.add(xline);
-
-const tlinematerial = new THREE.LineBasicMaterial({color: 0x6e6e6e});
-const tpoints = [];
-const tlinegeometry = new THREE.BufferGeometry().setFromPoints( tpoints );
-const tline = new THREE.Line( tlinegeometry, tlinematerial );
-scene.add(tline);
 const loader = new FontLoader();
 
 loader.load( sevseg, function ( font ) {
@@ -170,10 +177,10 @@ loader.load( sevseg, function ( font ) {
 } );
 
 
-function convertRange( value, r1, r2 ) { 
+/*function convertRange( value, r1, r2 ) { 
     return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ];
 }
-/*function tellPos(p){
+function tellPos(p){
   const lookmvmt = 5;
   console.log('Position X : ' + p.pageX + ' Position Y : ' + p.pageY);
   camera.rotation.y = 0.01 * convertRange(p.pageX, [0,1500], [lookmvmt, -lookmvmt]);
@@ -190,75 +197,22 @@ moveCamera();
 
 function animate() {
   requestAnimationFrame(animate);
-
-  sun.rotation.x += 0.001;
-  sun.rotation.y += 0.0035;
-  sun.rotation.z += 0.002;
-
-  earthTick += 0.01;
-  let newEarthX = 50*Math.sin(earthTick);
-  let newEarthY = 50*Math.cos(earthTick);
-  let newEarthZ = 10*Math.sin(earthTick);
-  earth.position.set(newEarthX, newEarthY, newEarthZ);
-  earth.rotation.x -= 0.001;
-  earth.rotation.y += 0.0035;
-  earth.rotation.z -= 0.002;
- 
-  planetxTick += 0.003;
-  let newPlanetxX = 10*Math.sin(earthTick);
-  let newPlanetxY = -100*Math.sin(earthTick);
-  let newPlanetxZ = 100*Math.cos(earthTick);
-  planetx.position.set(newPlanetxX, newPlanetxZ, newPlanetxY);
-  planetx.rotation.x += 0.003;
-  planetx.rotation.y -= 0.01;
-  planetx.rotation.z -= 0.002;
   
-  planettTick += 0.008;
-  let newPlanettX = -25*Math.sin(planettTick);
-  let newPlanettY = -25*Math.cos(planettTick);
-  let newPlanettZ = 50*Math.cos(planettTick);
-  planett.position.set(newPlanettX, newPlanettY, newPlanettZ);
-  planett.rotation.x -= 0.002;
-  planett.rotation.y -= 0.001;
-  planett.rotation.z -= 0.03;
+  sun.rotate(0.01, -0.0025, -0.007);
+  earth.rotate(-0.001, 0.0035, -0.002);
+  planetx.rotate(0.003, -0.01, -0.002);
+  tiny.rotate(-0.002, -0.001, -0.03);
 
-  epoints.push( new THREE.Vector3( earth.position.x, earth.position.y, earth.position.z));
-  eline.geometry = new THREE.BufferGeometry().setFromPoints(epoints);
-  scene.add(eline);
-  elinecount += 1;
-  if ( elinecount > 50 ) {
-    epoints.shift();
-  } 
+  earth.orbit(50, 'sin', 'cos', 'sin', tick, 100);
+  planetx.orbit(100, 'sin', 'sin', 'cos', tick, 200);
+  tiny.orbit(25, 'cos', 'sin', 'sin', tick, 75);
   
-  xpoints.push( new THREE.Vector3( planetx.position.x, planetx.position.y, planetx.position.z));
-  xline.geometry = new THREE.BufferGeometry().setFromPoints(xpoints);
-  scene.add(xline);
-  xlinecount += 1;
-  if ( xlinecount > 70 ) {
-    xpoints.shift();
-  } 
+  earth.updateTrail(50);
+  planetx.updateTrail(70);
+  tiny.updateTrail(35);
   
-  tpoints.push( new THREE.Vector3( planett.position.x, planett.position.y, planett.position.z));
-  tline.geometry = new THREE.BufferGeometry().setFromPoints(tpoints);
-  scene.add(tline);
-  tlinecount += 1;
-  if ( tlinecount > 35 ) {
-    tpoints.shift();
-  }
-
-  //camera.position.x = 0;
-  //camera.position.y = 0;
-  //camera.position.z = 175;
-
-  //controls.update();
-  //renderer.render(scene, camera);
+  tick += 1.5;
   composer.render();
 }
-let elinecount = 0;
-let xlinecount = 0;
-let tlinecount = 0;
-let earthTick = 0;
-let planetxTick = 0;
-let planettTick = 0;
-let camTick = 0;
+let tick = 0;
 animate();
